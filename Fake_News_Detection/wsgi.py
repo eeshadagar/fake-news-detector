1b2db3397c22e848b580c100b27e78961008ef38
Eesha Dagar <dagar.eesha75@yahoo.com>
2025-02-10 11:25:54 +0530

    fixed issue

--- Fake_News_Detection/wsgi.py
+++ Fake_News_Detection/wsgi.py
@@ -1,14 +1,8 @@
-"""
-WSGI config for Fake_News_Detection project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-from whitenoise import WhiteNoise
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise  
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fake_News_Detection.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Fake_News_Detection.settings")
+
+application = get_wsgi_application()
 application = WhiteNoise(application)
